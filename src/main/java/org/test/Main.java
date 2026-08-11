e0156da8413226a5f6edf2dc4b39b5f39a7617ad
NgocAnh1502 <ngocanhtruong2k4@gmail.com>
2026-08-11 10:24:31 +0700

    Commit Main.java

---
+++ src/main/java/org/test/Main.java
@@ -0,0 +1,12 @@
+package org.test;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap ten: ");
+        String ten =  sc.nextLine();
+        System.out.print("Hello " + ten);
+    }
+}
