d9a1d4fb684c6305de95a9c50be8b7219600f24e
NgocAnh1502 <ngocanhtruong2k4@gmail.com>
2026-08-12 14:43:22 +0700

    Commit Collection

---
+++ src/main/java/org/collection/listName.java
@@ -0,0 +1,22 @@
+package org.collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class listName {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+
+        names.add("Nguyen Van B");
+        names.add("Nguyen Thi A");
+        names.add("Tran Van D");
+        names.add("Tran Thi C");
+
+        Collections.sort(names);
+
+        for(String name : names){
+            System.out.println(name);
+        }
+    }
+}
